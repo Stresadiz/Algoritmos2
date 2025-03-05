2b1fe5b1b2094ac66b945da04248530a1d2f679a
Sebastian Luna <eliaslunaslo@gmail.com>
2025-03-05 18:30:37 -0300

    Commit inicial de Apuntes

---
+++ Algoritmos-Semana01/Cuadrado.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos_Semana01
+{
+    public class Cuadrado
+    {
+        public double lado;
+        public double perimetro;
+        public double area;
+
+        public Cuadrado(double lado)
+        {
+            this.lado = lado;
+            perimetro = lado*4;
+            area = lado*lado;
+        }
+
+        override
+        public string ToString()
+        {
+            string msj = "";
+            msj += "El valor del perimetro del cuadrado es:" + perimetro + Environment.NewLine;
+            msj += "El valor del area del cuadrado es:" + area + Environment.NewLine;
+
+            return msj;
+
+        }
+    }
+}
